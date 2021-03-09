852d46789454fdf1d4d0e24b8a211b8397493199
Станислав Алтынбаев <ASDragoon@yandex.ru>
2021-03-09 23:34:15 +0300

    Added auth with JWT tokens. Added CORS. Added quards.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,27 @@ import { OrderModule } from './order/order.module';
 import { CustomerModule } from './customer/customer.module';
 import { EmployeeModule } from './employee/employee.module';
 import { MaterialModule } from './material/material.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from './controllers/app.controller';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
       autoSchemaFile: 'schema.gql',
+      cors: {
+        origin: true,
+        credentials: true,
+      },
     }),
     OrderModule,
     CustomerModule,
     EmployeeModule,
     MaterialModule,
+    AuthModule,
+    UsersModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
