bed09b92d814db39738cc2c61b53d1cebaf30661
Llirik1337 <aleshinwork.1337@gmail.com>
2021-04-13 12:57:33 +0300

    added created table

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { OrderDocument } from 'src/order/entities/order.entity';
 import { OrderService } from 'src/order/order.service';
-import { Document, Packer, Paragraph, Table, TableCell, TableRow } from 'docx';
-import { CreateDocumentInput } from './dto/create-document.input';
-import { UpdateDocumentInput } from './dto/update-document.input';
+import {
+  Document,
+  Packer,
+  Paragraph,
+  Table,
+  TableCell,
+  TableRow,
+  TextDirection,
+  TextRun,
+  VerticalAlign,
+  WidthType,
+} from 'docx';
+import { OrderComponentDocument } from 'src/order-component/entities/order-component.entity';
 
 @Injectable()
 export class DocumentsService {
@@ -27,24 +37,186 @@ export class DocumentsService {
     }
   }
 
+  getDocumentTableHeader() {
+    const number = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: '№ п/п', bold: true })],
+        }),
+      ],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+
+    const componentName = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: 'Наименование детали', bold: true })],
+        }),
+      ],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+
+    const count = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: 'Кол-во, шт.', bold: true })],
+        }),
+      ],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    const costOfOneWithOutNDC = new TableCell({
+      children: [
+        new Paragraph({
+          children: [
+            new TextRun({ text: 'Цена за ед. без НДС, руб', bold: true }),
+          ],
+        }),
+      ],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    const costWithOutNDC = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: 'Сумма без НДС, руб.', bold: true })],
+        }),
+      ],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    const costNDC = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: 'НДС 20%, руб.', bold: true })],
+        }),
+      ],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    const costWithNDC = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: 'Сумма с НДС, руб.', bold: true })],
+        }),
+      ],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+
+    const header = new TableRow({
+      children: [
+        number,
+        componentName,
+        count,
+        costOfOneWithOutNDC,
+        costWithOutNDC,
+        costNDC,
+        costWithNDC,
+      ],
+    });
+
+    return header;
+  }
+
+  getComponentRow(orderComponent: OrderComponentDocument) {
+    const componentName = new TableCell({
+      children: [new Paragraph(orderComponent.component.name)],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+
+    const count = new TableCell({
+      children: [new Paragraph(String(orderComponent.count))],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    const costOfOneWithOutNDC = new TableCell({
+      children: [new Paragraph(String(orderComponent.component.cost))],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    const costWithOutNDC = new TableCell({
+      children: [new Paragraph(String(orderComponent.cost))],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+
+    const fullCost = orderComponent.cost * 1.2;
+
+    const costWithNDC = new TableCell({
+      children: [new Paragraph(String(fullCost))],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    const costNDC = new TableCell({
+      children: [new Paragraph(String(fullCost - orderComponent.cost))],
+      verticalAlign: VerticalAlign.CENTER,
+    });
+    return [
+      componentName,
+      count,
+      costOfOneWithOutNDC,
+      costWithOutNDC,
+      costNDC,
+      costWithNDC,
+    ];
+  }
+
+  getResultRow(order: OrderDocument) {
+    const result = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: 'Итого', bold: true })],
+        }),
+      ],
+      columnSpan: 4,
+    });
+
+    const costWithOutNDC = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: String(order.cost), bold: true })],
+        }),
+      ],
+    });
+
+    const fullCost = order.cost * 1.2;
+
+    const costWithNDC = new TableCell({
+      children: [
+        new Paragraph({
+          children: [new TextRun({ text: String(fullCost), bold: true })],
+        }),
+      ],
+    });
+    const costNDC = new TableCell({
+      children: [
+        new Paragraph({
+          children: [
+            new TextRun({ text: String(fullCost - order.cost), bold: true }),
+          ],
+        }),
+      ],
+    });
+    return [result, costWithOutNDC, costNDC, costWithNDC];
+  }
+
   getDocument(order: OrderDocument) {
+    const rows = order.orderComponents.map(
+      (orderComponent, index) =>
+        new TableRow({
+          children: [
+            new TableCell({
+              children: [
+                new Paragraph({ children: [new TextRun(String(index + 1))] }),
+              ],
+            }),
+            ...this.getComponentRow(orderComponent),
+          ],
+        }),
+    );
+
     const document = new Document({
       sections: [
         {
           children: [
             new Table({
+              columnWidths: [500, 2000, 500, 1000, 1000, 1000, 1000],
               rows: [
-                new TableRow({
-                  children: [
-                    new TableCell({
-                      children: [
-                        new Paragraph({
-                          text: 'Hello world',
-                        }),
-                      ],
-                    }),
-                  ],
-                }),
+                this.getDocumentTableHeader(),
+                ...rows,
+                new TableRow({ children: this.getResultRow(order) }),
               ],
             }),
           ],
